3ce070e1b9759afbc623e6dfd2556e833c68b535
saifullah-10 <saifullah.bus@gmail.com>
2024-08-17 10:17:21 +0600

    update cors

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ require("dotenv").config();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://ecom-store-tau-seven.vercel.app",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
